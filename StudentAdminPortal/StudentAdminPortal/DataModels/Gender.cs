4b3c90a981b8ed233c611c0b6230abf7f8a6cfae
Shweta Navkar <shwetanavkar18@gmail.com>
2022-06-17 14:51:00 +0530

    Added models and migration

---
+++ StudentAdminPortal/StudentAdminPortal/DataModels/Gender.cs
@@ -0,0 +1,8 @@
+﻿namespace StudentAdminPortal.API.DataModels
+{
+    public class Gender
+    {        
+        public Guid ID { get; set; }
+        public string Description { get; set; }
+    }
+}
--- StudentAdminPortal/StudentAdminPortal/Program.cs
+++ StudentAdminPortal/StudentAdminPortal/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using StudentAdminPortal.API.DataModels;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<StudentAdminContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("StudentAdminPortalDB")));
+//builder.Services.AddScoped<IStudentRepository, SqlStudentRepository>();
+
 
 var app = builder.Build();
 
